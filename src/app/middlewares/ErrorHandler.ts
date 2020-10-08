c92b491863be1383a3591dc3befebceaa8a36706
Felipe <felipeas314@gmail.com>
2020-10-08 16:12:03 -0300

    criação dos middlewares

--- src/app/middlewares/ErrorHandler.ts
+++ src/app/middlewares/ErrorHandler.ts
@@ -6,7 +6,11 @@ export class ErrorHandler implements ExpressErrorMiddlewareInterface {
     error(error: any, request: any, response: any, next: (err: any) => any) {
         console.log("ErrorHandler...");
         console.log(error)
-        return response.status(400).json('error');
+        if(error){
+            return response.status(400).json('error');
+        }
+
+        return next();
     }
 
 }
---
+++ src/app/middlewares/LoggingMiddleware.ts
@@ -0,0 +1,11 @@
+import { Middleware, ExpressMiddlewareInterface } from "routing-controllers";
+
+@Middleware({ type: "before" })
+export class LoggingMiddleware implements ExpressMiddlewareInterface {
+
+    use(request: any, response: any, next: (err: any) => any): void {
+        console.log("do something...");
+        next();
+    }
+
+}
